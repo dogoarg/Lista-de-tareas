c33c5968b247fba07c6a846c17f713cce3c5d84c
Mariano Araoz Teran <113387906+dogoarg@users.noreply.github.com>
2023-05-31 12:29:05 -0300

    Lista-Tareas

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import dogo from './Imagenes/dogo.jpg'
+import ListaDeTareas from './componentes/ListaDeTareas';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="aplicacion-tareas">
+      <div className='logo-contenedor'>
+        <img src={dogo} className='logo'/>
+      </div>
+      <div className='tareas-lista-principal'>
+        <h1>Mis tareas</h1>
+        <ListaDeTareas/>
+      </div>
+      <div>
+      </div>
     </div>
   );
 }
---
+++ src/componentes/TareaFormulario.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import '../Hojas-de-estilos/TareaFormulario.css'
+import { v4 as uuidv4 } from 'uuid';
+
+function TareaFormulario(props){
+
+    const [input, setInput] = useState ('');
+
+    const manejarCambio = e => {
+       setInput(e.target.value)
+    }
+
+
+    const manejarEnvio = e =>{
+        e.preventDefault();
+        
+        const tareaNueva = {
+            id: uuidv4(),
+            texto:input,
+            completada: false
+        }
+        props.onSubmit(tareaNueva);
+    }
+
+
+    return(
+        <form className="tarea-formulario" onSubmit= {manejarEnvio}>
+        <input className="tarea-input" 
+        type="text"
+         placeholder="Escribe tu tarea" 
+        name="textp"
+        onChange={manejarCambio}/>
+        <button className='boton-tarea'>Agregar tarea</button>
+        </form>
+    )
+}
+
+export default TareaFormulario
